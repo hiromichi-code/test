5c97565a356d69576c02736273aae653d03eb796
hiromichi-code <hiromichi.s.617@gmail.com>
2020-10-12 12:54:02 +0900

    デザイン修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.scss';
-import image from './images/image.png';
+import image from './images/image.svg';
 import Button from './components/Button';
 import Footer from './components/Footer';
 
 const App = () => {
   return(
     <div className="title">
-      <h1>Shinohara BookBinding inc.</h1>
+      <h1>Mentsuke</h1>
+      <h2>Shinohara BookBinding inc.</h2>
       <p>Fukuoka.Japan</p>
-      <img src={image} alt='imagePicture'></img>
       <div className="btn">
         <Button />
       </div>
-        <Footer />
+      <img src={image} alt='imagePicture'></img>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
   return (
     <div className='copy'>
-      <div>&copy;shinohara</div>
+      <div className="copy-text">&copy;Shinohara BookBinding inc.</div>
     </div>
   );
 }
